43dc71b7df6538883cd211db9a8fc19c6f30ef40
Lucas Rodas <61189548+HeadIntoSound@users.noreply.github.com>
2024-06-14 19:14:35 -0300

    Solved all inconsistencies

--- js/memoria.js
+++ js/memoria.js
@@ -1,53 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
+    console.log("Working");
     //list all card options
     const cardArray = [
       {
         name: 'cruce caminos',
-        img: '../imagenes memoria/cruce de caminos.jpg'
+        img: '../images/Memoria/cruce de caminos.png'
       },
       {
         name: 'stop',
-        img: '../imagenes memoria/stop.png'
+        img: '../images/Memoria/stop.png'
       },
       {
         name: 'giro u',
-        img: '../imagenes memoria/girar en u copy.jpg'
+        img: '../images/Memoria/uturn.png'
       },
       {
         name: 'cruz San Andres',
-        img: '../imagenes memoria/cruz de san andres.jpg'
+        img: '../images/Memoria/cruz de san andres.png'
       },
       {
         name: 'curva peligrosa',
-        img: '../imagenes memoria/curva.jpg'
+        img: '../images/Memoria/curva.png'
       },
       {
         name: 'gomeria',
-        img: '../imagenes memoria/gomeria.png'
+        img: '../images/Memoria/gomeria.png'
       },
       {
         name: 'cruce caminos',
-        img: '../imagenes memoria/cruce de caminos.jpg'
+        img: '../images/Memoria/cruce de caminos.png'
       },
       {
         name: 'stop',
-        img: '../imagenes memoria/stop.png'
+        img: '../images/Memoria/stop.png'
       },
       {
         name: 'giro u',
-        img: '../imagenes memoria/girar en u copy.jpg'
+        img: '../images/Memoria/uturn.png'
       },
       {
         name: 'cruz San Andres',
-        img: '../imagenes memoria/cruz de san andres.jpg'
+        img: '../images/Memoria/cruz de san andres.png'
       },
       {
         name: 'curva peligrosa',
-        img: '../imagenes memoria/curva.jpg'
+        img: '../images/Memoria/curva.png'
       },
       {
         name: 'gomeria',
-        img: '../imagenes memoria/gomeria.png'
+        img: '../images/Memoria/gomeria.png'
       }
     ]
   
@@ -63,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function createBoard() {
       for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
-        card.setAttribute('src', 'images/imagenes memoria/images.jpg')
+        card.setAttribute('src', '../images/Memoria/images.png')
         card.setAttribute('data-id', i)
         card.addEventListener('click', flipCard)
         grid.appendChild(card)
@@ -77,20 +78,20 @@ document.addEventListener('DOMContentLoaded', () => {
       const optionTwoId = cardsChosenId[1]
       
       if(optionOneId == optionTwoId) {
-        cards[optionOneId].setAttribute('src', 'images/imagenes memoria/images.jpg')
-        cards[optionTwoId].setAttribute('src', 'images/imagenes memoria/images.jpg')
+        cards[optionOneId].setAttribute('src', '../images/Memoria/images.png')
+        cards[optionTwoId].setAttribute('src', '../images/Memoria/images.png')
         alert ('Seleccionaste dos veces la misma carta')
       }
       else if (cardsChosen[0] === cardsChosen[1]) {
         alert('Haz encontrado un par')
-        cards[optionOneId].setAttribute('src', 'images/imagenes memoria/blanco.png')
-        cards[optionTwoId].setAttribute('src', 'images/imagenes memoria/blanco.png')
+        cards[optionOneId].setAttribute('src', '../images/Memoria/blanco.png')
+        cards[optionTwoId].setAttribute('src', '../images/Memoria/blanco.png')
         cards[optionOneId].removeEventListener('click', flipCard)
         cards[optionTwoId].removeEventListener('click', flipCard)
         cardsWon.push(cardsChosen)
       } else {
-        cards[optionOneId].setAttribute('src', 'images/imagenes memoria/images.png')
-        cards[optionTwoId].setAttribute('src', 'images/imagenes memoria/images.png')
+        cards[optionOneId].setAttribute('src', '../images/Memoria/images.png')
+        cards[optionTwoId].setAttribute('src', '../images/Memoria/images.png')
         alert('Intentalo de nuevo')
       }
       cardsChosen = []
